92e3cca3f0011ed5fbc0fcce3211dccddcbae5f2
Maurice Thompson-Hamilton <mauricemlth@hotmail.com>
2023-07-25 16:33:06 +0100

    refactored the GameBoard class by moving thte GameEvents class to its own file.

--- Unity.MineSweeper/Assets/_Project/Scripts/GameBoard.cs
+++ Unity.MineSweeper/Assets/_Project/Scripts/GameBoard.cs
@@ -118,14 +118,4 @@ namespace BlackRece.MineSweeper {
             return validNeighbours;
         }
     }
-
-    public sealed class GameEvents : ScriptableObject {
-        public static event Func<Vector2Int, int> EvtMineCount;
-        public static event Action<Vector2Int> EvtRevealCell; 
-        public static event Action EvtRevealMines;
-
-        public static int OnMineCount(Vector2Int position) => EvtMineCount?.Invoke(position) ?? 0;
-        public static void OnRevealCell(Vector2Int position) => EvtRevealCell?.Invoke(position);
-        public static void OnRevealMines() => EvtRevealMines?.Invoke();
-    }
 }
